76a6fe76d14e6502618d908ac1ae09e90637ad32
nely de faria <nelly.nf54@gmail.com>
2021-07-28 19:33:20 -0300

    Inserindo api do openweather

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ document.querySelector('.busca').addEventListener('submit', (event)=> {
 
     if (input != '') {
         showWarning('Carregando...');
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=0b91301ae6107e00534ff53cdd3bd605&units=metric&lang=pt_br`;
     }
     
 });
